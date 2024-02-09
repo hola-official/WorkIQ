fafe3163f7423e7512b90c0c2cc7efc84a2c9d0a
muhammedola <141998553+muhammedola@users.noreply.github.com>
2024-02-09 15:52:32 +0100

    commit

--- server/server.js
+++ server/server.js
@@ -1,62 +1,75 @@
+// Import required modules
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const passport = require("passport");
 const cors = require("cors");
+const credentials = require("./middleware/credentials");
 const corsOptions = require("./config/corsOptions");
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
 
+// Load environment variables from .env file
 dotenv.config();
+
+// Set port number, defaulting to 3000 if not specified in environment variables
 const PORT = process.env.PORT || 3000;
 
+// Create an instance of Express application
 const app = express();
 
-// Set up MongoDB store
+// Set up MongoDB session store
 const store = new MongoDBStore({
-	uri: process.env.MONGO_URI,
-	collection: "sessions",
-	expires: 1 * 60 * 60, // 1hr in seconds
+    uri: process.env.MONGO_URI, // MongoDB connection URI
+    collection: "sessions", // Collection to store sessions
+    expires: 1 * 60 * 60, // Session expiration time in seconds (1 hour)
 });
 
-
-// Handle options credentials check - before CORS!
-// and fetch cookies credentials requirement
+// Middleware to handle options credentials check and fetch cookies credentials requirement
 app.use(credentials);
 
-// Cross Origin Resource Sharing
+// Middleware for parsing JSON data in request body
+app.use(express.json({ limit: "50mb" }));
 
-app.use(express.json({ limit: "50mb" })); //parse json data inside the req body
-app.use(express.urlencoded({ extended: true })); // parse form data inside the req body
+// Middleware for parsing URL-encoded data in request body
+app.use(express.urlencoded({ extended: true }));
+
+// Enable Cross-Origin Resource Sharing (CORS) with custom options
 app.use(cors(corsOptions));
+
+// Middleware for parsing cookies
 app.use(cookieParser());
 
-// Use express-session middleware
+// Set up express-session middleware for session management
 app.use(
-	session({
-		secret: process.env.JWT_SECRET, // Change this to a secure secret key
-		resave: true,
-		saveUninitialized: false,
-		store: store, // Use MongoDB store
-		cookie: {
-			maxAge: 1 * 60 * 60 * 1000, // 1hr in milliseconds,
-			secure: true, // Set to true if using HTTPS
-			sameSite: "none",
-		},
-	})
+    session({
+        secret: process.env.JWT_SECRET, // Secret key used to sign the session ID cookie
+        resave: true, // Forces session to be saved even if not modified
+        saveUninitialized: false, // Prevents saving uninitialized sessions
+        store: store, // Use MongoDB store for session storage
+        cookie: {
+            maxAge: 1 * 60 * 60 * 1000, // Session expiration time in milliseconds (1 hour)
+            secure: true, // Set to true if using HTTPS
+            sameSite: "none", // Enforces same-site cookie attribute for CSRF protection
+        },
+    })
 );
 
+// Define a route for the home page
 app.get("/", (req, res) => {
-  res.send("Home Page");
+    res.send("Home Page");
 });
 
-
+// Initialize Passport authentication middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Connect to MongoDB database
 mongoose
-	.connect(process.env.MONGO_URI)
-	.then(() => {
-		app.listen(PORT, () => console.log(`Server Is 🏃‍♂️ On ` + PORT));
-	})
-	.catch((err) => console.log(err));
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        // Start the server and listen for incoming connections
+        app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+    })
+    .catch((err) => console.log(err));
+		
